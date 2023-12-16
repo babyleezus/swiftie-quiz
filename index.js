b2ae9fe9263a41e57cec7a3619e9ef06af4742ae
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-12-16 01:05:12 +0000

    added initial js variables

---
+++ index.js
@@ -0,0 +1,8 @@
+let heading = document.getElementById('heading')
+let tagline = document.getElementById('tagline')
+let startBtn = document.getElementById('start-btn')
+let nextBtn = document.getElementById('next-btn')
+
+startBtn.addEventListener('click', () =>{
+    
+})
